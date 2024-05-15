beb174c3de922b05bee839fbecc3b24505698c7a
Daniel Monyak <danmonyak@gmail.com>
2024-05-15 18:42:59 -0400

    updates may

--- Process_Other_Data_Sources/Lund.py
+++ Process_Other_Data_Sources/Lund.py
@@ -32,20 +32,36 @@ print('Loaded.')
 output_dir = os.path.join('outputs', 'cohort_T2')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
+    
+print(f'{clinical.shape[0]} tumors initially')
 
 # Filter by LUMP
 # Set in_CpG_dataset and reason_purity flags accordingly - based on LUMP value
 # Remove impure tumors from beta_values
 LUMP_purity = m_util.getLUMP_values(beta_values)
-clinical['in_CpG_dataset'] = clinical.index.isin(LUMP_purity.index[LUMP_purity >= LUMP_THRESH])
-clinical['LUMP'] = LUMP_purity         # need to add in LUMP column also - extra change
-clinical['reason_purity'] = ~clinical['in_CpG_dataset']
+clinical = clinical.merge(LUMP_purity.rename('LUMP'), left_index=True, right_index=True)
+clinical['reason_purity'] = clinical['LUMP'] < LUMP_THRESH
+clinical['in_CpG_dataset'] = ~clinical['reason_purity']
+print(f'{clinical["reason_purity"].sum()} tumors removed for having LUMP < {LUMP_THRESH}')
+
+clinical['reason_primary'] = clinical['primary'] != 'primary'
+clinical['in_CpG_dataset'] &= ~clinical['reason_primary']
+print(f'{clinical["reason_primary"].sum()} tumors removed for not being primary tumors')
+
+filt = clinical.loc[clinical['in_CpG_dataset']]
+clinical = clinical.merge(filt['age'].duplicated().rename('reason_duplicate'), left_index=True, right_index=True)
+clinical['reason_duplicate'] = clinical['reason_duplicate'].fillna(False)
+clinical['in_CpG_dataset'] &= ~clinical['reason_duplicate']
+print(f'{clinical["reason_duplicate"].sum()} tumors removed for being from the same patient as a previous one.')
+
 beta_values = beta_values[clinical.index[clinical['in_CpG_dataset']]]
+print(f'{clinical["in_CpG_dataset"].sum()} tumors kept')
 
 # Set in_analysis_dataset flag to indicate that tumor is ductal or not ductal
 clinical['in_analysis_dataset'] = clinical['in_CpG_dataset'] & (clinical['tumorType_loRes']=='ductal')
 clinical.to_csv(os.path.join(proj_dir, 'cohort.T2.clinical.txt'), sep='\t')
 
+print(f'{clinical["in_analysis_dataset"].sum()} ductal tumors kept for analysis')
 print('Saved modified clinical file.')
 
 # Calculate and save c_beta values for each tumor
@@ -55,3 +71,6 @@ c_beta.name = 'c_beta'
 c_beta.to_csv(os.path.join(output_dir, 'cohort.T2.c_beta.txt'), sep='\t')
 
 print('Saved c_beta value of each tumor.')
+
+# Don't need to remove any samples for having >= 5% missing values in Clock fCpGs
+assert (beta_values.loc[balanced_CpGs].isna().mean(axis=0) < 0.05).all()
