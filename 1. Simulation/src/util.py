7863572317cd489808c6531b0556b56c691fbd44
Daniel Monyak <danmonyak@gmail.com>
2025-12-28 01:39:28 -0500

    added fucntionality for when the lists are empty

--- 1. Simulation/src/util.py
+++ 1. Simulation/src/util.py
@@ -5,6 +5,8 @@ def arr2dToString(arr):
     return '\n'.join(['\t'.join(map(lambda x: format(x, '.18e'), x)) for x in arr]) + '\n'
 
 def writeBetaValues(beta_list, beta_values_outfilepath, should_clear=True):
+    if len(beta_list) == 0:
+        return
     with open(beta_values_outfilepath, 'a') as f:
         beta_arr = np.stack(beta_list, axis=0)
         beta_arr_str = arr2dToString(beta_arr)
@@ -13,6 +15,8 @@ def writeBetaValues(beta_list, beta_values_outfilepath, should_clear=True):
         beta_list.clear()
 
 def writeNcells(n_cells_list, n_cells_outfilepath, should_clear=True):
+    if len(n_cells_list) == 0:
+        return
     with open(n_cells_outfilepath, 'a') as f:
         n_cells_arr = np.stack(n_cells_list, axis=0)
         n_cells_arr_str = arr2dToString(n_cells_arr)
